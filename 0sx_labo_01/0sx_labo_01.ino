ab1e248cc00496675076e14a72b02fb8536a95e9
allen <allendje@yahoo.com>
2025-01-27 15:59:18 -0500

    projet presque fini

--- 0sx_labo_01/0sx_labo_01.ino
+++ 0sx_labo_01/0sx_labo_01.ino
@@ -1,4 +1,8 @@
+// la led qui sera utilisée
 const int LED = 13;
+// Des variables qui seront utilisées dans l'état de variation
+int brightness = 0;
+int fade = 5;
 
 void setup(){
   Serial.begin(9600);
@@ -6,7 +10,7 @@ void setup(){
 }
 
 void loop(){
-
+  etatAllumer();
 }
 
 void etatAllumer(){
@@ -18,11 +22,27 @@ void etatAllumer(){
 }
 
 void etatClignotement(){
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
+  Serial.println("Clignotement - 2201250");
+  for (int i = 0; i < 4; i++){
+    digitalWrite(LED, HIGH);
+    delay(250);
+    digitalWrite(LED, LOW);
+    delay(250);
+  }
+  etatVariation();
 }
-void etatVariation(){
 
+void etatVariation(){
+  Serial.println("Variation - 2201250");
+  analogWrite(LED, brightness);
+  brightness = brightness + fade;
+  while (brightness <= 0 || brightness >= 255){
+    fade = -fade;
+  }
+  cycly();
+}
+
+void cycly(){
+  Serial.println("Éteint - 2201250");
+  delay(2000);
 }
